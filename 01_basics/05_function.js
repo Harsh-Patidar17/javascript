86e90a6af964c8d1b420dae43140a6108af58be6
Harsh-Patidar17 <harshpatidar998@gmail.com>
2025-02-22 14:35:39 +0530

    dddddd

---
+++ 01_basics/05_function.js
@@ -0,0 +1,16 @@
+const myFunction=function(){
+    console.log("harsh")
+}
+myFunction();
+
+
+
+
+let userOne={
+    email:"harsh@123",
+}
+let userTwo=userOne
+userTwo.email="ohhman"
+
+console.log(userOne.email);
+console.log(userTwo.email);
