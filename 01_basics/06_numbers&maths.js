797e228c5eec293dbd64c4d8e967eaf44813e19c
Harsh-Patidar17 <harshpatidar998@gmail.com>
2025-02-23 15:38:02 +0530

    ohh yeah

---
+++ 01_basics/06_numbers&maths.js
@@ -0,0 +1,24 @@
+const score =100;
+console.log(score);
+
+const balance =new Number(123.4)
+console.log(balance);
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNamw=123.865
+console.log(otherNamw.toPrecision(3));
+
+const hundereds=100000000
+console.log(hundereds.toLocaleString('en-IN'))
+
+// Math Library //
+
+console.log(Math.round(4.6))
+console.log((Math.random()*10)+1)
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
