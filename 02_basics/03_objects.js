9618dbd1bb3215681b78a4bd7fe562557b677ed1
Harsh-Patidar17 <harshpatidar998@gmail.com>
2025-02-25 23:14:37 +0530

    damn

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,2 +1,24 @@
-let harsh =3;
+const mySym=Symbol("key1")
+const Juser={
+    name: "harsh",
+    age: 20,
+    location: "nagpur",
+    email:"harsh@google.com",
+    "dayum":"damn",
+    isLoggedIN:false,
+    lastLoginDays:["monday","sunday"],
+    [mySym]:"mykey1"// to refer symbol we have to use this 
+}
+console.log(Juser.email);
+console.log(Juser["dayum"]);
+console.log(Juser[mySym]);
 
+Juser.email="damn_patidar"
+//Object.freeze(Juser)
+Juser.email="okay"
+//console.log(Juser)
+
+Juser.greeting=function(){
+    console.log("gand marao")// we can see undefined in terminal , the reason is not known till now will get to know afterwards 
+}
+console.log(Juser.greeting())
